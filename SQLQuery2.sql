dffc6731a87dbf7405236596d01db1d454b3393a
[vedhashni] <[vedhavel.1234@gmail.com]>
2021-08-03 19:28:19 +0530

    [Vedhashni] Updated the file with new columns

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -8,7 +8,7 @@ use payroll_service;
 
 --------------UC2--------------
 
---------Create Tabel------------
+--------Create Table------------
 
 create table employee_payroll
 (
@@ -23,17 +23,17 @@ emailId varchar(20)
 
 Insert into employee_payroll values ('Ashok',70000,'2021-05-15','ashok32@gmail.com'),('Karthick',55000,'2021-02-13','karthi21@gmail.com'),('Gayatri',45000,'2021-07-13','gaya@gmail.com'),('Divya',30000,'2021-08-23','divya2000@gmail.com');
 
--------------------UC4--------------------
+-------------------UC4 Retrive data by select statement--------------------
 
 select * from employee_payroll;
 
-------------------UC5----------------
+------------------UC5 Retriveing particular data by condition----------------
 
 select name,salary from employee_payroll where name='Karthick';
 
 select name,salary from employee_payroll where startDate between ('2021-02-01') and getdate();
 
-------------------UC6---------------------
+------------------UC6 Altered the table to add gender column---------------------
 
 alter table employee_payroll add Gender char(1);
 
@@ -41,7 +41,7 @@ update employee_payroll set Gender='M' where name='Ashok' or name='Karthick';
 
 update employee_payroll set Gender='F' where name='Gayatri' or name='Divya';
 
--------------------UC7--------------------------------
+-------------------UC7 Using Aggregate Functions and GroupBy--------------------------------
 
 select sum(salary) as TotalSalary from employee_payroll;
 
@@ -57,3 +57,26 @@ select count(salary) as CountofGender ,gender from employee_payroll   group by g
 
 select max(salary) as maxSalary ,gender from employee_payroll   group by gender;
 
+----------------------UC8 Adding phone number, department and address columns-----------------
+
+alter table employee_payroll add PhoneNumber bigint;
+
+alter table employee_payroll add Department varchar(25) not null default 'HR';
+
+update employee_payroll set PhoneNumber=8945125478 where name='Ashok';
+update employee_payroll set PhoneNumber=9845127810 where name='Karthick';
+update employee_payroll set PhoneNumber=8745123654 where name='Gayatri';
+update employee_payroll set PhoneNumber=9874512458 where name='Divya';
+
+
+
+alter table employee_payroll add Address varchar(25) default 'Not Provided';
+
+
+update employee_payroll set Address='Chennai' where name='Ashok';
+update employee_payroll set Address='Bangalore' where name='Karthick';
+update employee_payroll set Address='Cochin' where name='Gayatri';
+update employee_payroll set Address='Hyderbad' where name='Divya';
+
+
+select * from employee_payroll;
